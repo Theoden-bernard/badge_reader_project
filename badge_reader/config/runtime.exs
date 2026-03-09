6aaf923495f92c8a34f90ac1c93770b2b272050b
theoden <theodenbernard1203@gmail.com>
2026-03-10 00:14:48 +0100

    fix: edit dockerfile #28

--- badge_reader/config/runtime.exs
+++ badge_reader/config/runtime.exs
@@ -35,7 +35,7 @@ if config_env() == :prod do
 
   config :badge_reader, BadgeReader.Repo,
     # ssl: true,
-    url: System.get_env("postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/${POSTGRES_DB}"),
+    url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     # For machines with several cores, consider starting multiple pools of `pool_size`
     # pool_count: 4,
