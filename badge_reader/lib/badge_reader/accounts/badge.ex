ef2ff81cc714e84bccfeaa245f8427ab3dab696b
theoden <theodenbernard1203@gmail.com>
2026-04-21 12:42:32 +0200

    feat: Implementation of the latest code elements and links to the GitHub repository #2

--- badge_reader/lib/badge_reader/accounts/badge.ex
+++ badge_reader/lib/badge_reader/accounts/badge.ex
@@ -2,6 +2,9 @@ defmodule BadgeReader.Accounts.Badge do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias BadgeReader.Repo
+  alias BadgeReader.Badges.Badge
+
   schema "badges" do
     field :rfid, :integer
     field :name_badge, :string
@@ -20,5 +23,9 @@ defmodule BadgeReader.Accounts.Badge do
     |> cast(attrs, [:rfid, :name_badge, :date_activation, :date_expiration, :statue])
     |> validate_required([:rfid, :name_badge, :date_activation, :date_expiration, :statue])
     |> put_change(:user_id, user_scope.user.id)
+  end
+
+  def delete_all_user_badges(user_id) do
+    Repo.delete_all(from b in Badge, where: b.user_id == ^user_id)
   end
 end
