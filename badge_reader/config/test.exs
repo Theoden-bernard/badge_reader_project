71277f55aa48947e523d7e2ce34d70b520f4d419
theoden <theodenbernard1203@gmail.com>
2026-06-30 13:55:14 +0200

    change ci/cd and dockerfile

--- badge_reader/config/test.exs
+++ badge_reader/config/test.exs
@@ -12,7 +12,7 @@ config :badge_reader, BadgeReader.Repo,
   username: System.get_env("POSTGRES_USER"),
   password: System.get_env("POSTGRES_PASSWORD"),
   hostname: "localhost",
-  database: System.get_env("POSTGRES_DB"),
+  database: "badge_reader_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
