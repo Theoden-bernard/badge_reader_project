e23d0d194abf815c8dc0e11d90c21edcace96f98
theoden <theodenbernard1203@gmail.com>
2026-06-30 13:42:47 +0200

    change ci/cd and dockerfile

--- badge_reader/config/test.exs
+++ badge_reader/config/test.exs
@@ -9,10 +9,10 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :badge_reader, BadgeReader.Repo,
-  username: "theoden",
-  password: "postgres",
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
   hostname: "localhost",
-  database: "badge_reader_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: System.get_env("POSTGRES_DB"),
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
